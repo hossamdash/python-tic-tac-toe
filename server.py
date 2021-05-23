8b6fae5d1259c7587cce411f7f1eb15aaa4cceb3
Hossam Mostafa <hossamdash2@gmail.com>
2021-05-23 16:48:14 +0200

    Initial two player support

---
+++ server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+import sys
+
+
+# Server IP and port
+IP = "127.0.0.1"
+PORT = 55555
+
+# Socket type and options
+server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+# dictionary of client sockets and their nicknames
+clients = {}
+
+# debuggin
+print(f"Listening for connections on {IP}:{PORT}...")
+
+
+# Sending Messages To All Connected Clients
+def broadcast(message, client_socket):
+    # Send messages to all clients except to the original sender
+    for client in clients.keys():
+        if client is not client_socket:
+            client.send("{} : {}".format(
+                clients[client], message).encode("utf-8"))
+
+
+# Function to be called per client
+def handle(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode("utf-8")
+        print(message)
+        broadcast(message, client_socket)
+        if "!exit" in message:
+            client_socket.close()
+            broadcast("{} left!".format(clients[client_socket]), client_socket)
+            clients.pop(client_socket)
+            print(clients)
+            sys.exit()
+
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client_socket, address = server_socket.accept()
+        print("Connected with {}".format(str(address)))
+
+        # # Request And Store Nickname
+        # client_socket.send("/id".encode("utf-8"))
+        # nickname = client_socket.recv(1024).decode("utf-8")
+        nickname = 'player 2'
+        # Add client info to the dictionary
+        clients.update({client_socket: nickname})
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client_socket,))
+        thread.start()
+
+
+receive()
--- ui.py
+++ ui.py
@@ -2,6 +2,33 @@ import sys
 from PySide2.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import Qt
+import socket, threading
+
+
+class SocketChat:
+    def __init__(self):
+        self.nickname = "Hossam"
+        # Server Ip and Port
+        self.IP = "127.0.0.1"
+        self.PORT = 55555
+        self.client_socket = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_STREAM
+        )
+        self.connection = True
+
+    def receive(self):
+        message = self.client_socket.recv(1024).decode("utf-8")
+        return message
+
+    def write(self, msg: str):
+        message = msg
+        self.client_socket.send(message.encode("utf-8"))
+        if message.startswith("/"):
+            self.handleCommand(message[1:])
+
+    def handleCommand(self, command: str):
+        if command == "exit":
+            return 404  # status code for exit
 
 
 class Example(QWidget):
@@ -23,6 +50,10 @@ class Example(QWidget):
         self.initUI()
         self.x_score = 0
         self.y_score = 0
+        self.chat_object = SocketChat()
+        self.chat_object.client_socket.connect(
+            (self.chat_object.IP, self.chat_object.PORT)
+        )
 
     def initUI(self):
         self.game_size = 3
@@ -107,6 +138,9 @@ class Example(QWidget):
             self.turn = "O"
         else:
             self.turn = "X"
+        message = self.chat_object.receive()
+        index = ''.join(char for char in message if char.isdigit())
+        self.buttons[index[0], index[1]].setText(self.turn)
         self.turn_label.setText("{}\nTurn".format(self.turn))
         self.checkGame()
 
@@ -114,6 +148,13 @@ class Example(QWidget):
         def action():
             if button.text() == "":
                 button.setText(self.turn)
+                tuple = [
+                    (index, row.index(button))
+                    for index, row in enumerate(self.buttons)
+                    if button in row
+                ]
+                print(tuple)
+                self.chat_object.write(str(tuple))
                 self.endTurn()
 
         return action
